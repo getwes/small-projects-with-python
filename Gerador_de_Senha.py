bef07b20a24d6ba478435daff914057155dc043b
Wesley Freire <wesleyfreire1707@gmail.com>
2023-03-14 14:26:03 -0300

    finalizado

--- Gerador_de_Senha.py
+++ Gerador_de_Senha.py
@@ -9,7 +9,7 @@ composicao = cmax + cmin + num + cEsp
 
 digitos = 15
 
-senha1 = "".join(random.sample(composicao,digitos));
+senha1 = "".join(random.sample(composicao,digitos)); # join esta fazendo a mesclagem das variaveis e o random.sample coletando dados da composição e delimitando quantidade de caracter.
 senha2 = "".join(random.sample(composicao,digitos));
 senha3 = "".join(random.sample(composicao,digitos));
 
